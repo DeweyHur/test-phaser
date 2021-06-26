3f166d8c01398880c1f50ae9a7c0cea218f0a473
Dewey Hur <digitzetre@gmail.com>
2021-06-25 21:12:07 -0700

    Collide (Part 1)

--- src/stage.tsx
+++ src/stage.tsx
@@ -13,15 +13,17 @@ const Stage = () => {
             scene.load.audio('bgm', ['assets/audio/BGM14.mp3']);
         });
         Create.on((scene: Scene) => {
-            const map = scene.make.tilemap({ key: 'tilemap' });
-            const tileset = map.addTilesetImage('istanbul', 'base_tiles');
+            const map: Phaser.Tilemaps.Tilemap = scene.make.tilemap({ key: 'tilemap' });
+            const tileset: Phaser.Tilemaps.Tileset = map.addTilesetImage('istanbul', 'base_tiles');
             map.createLayer('entrance', tileset);
             map.createLayer('maptile', tileset);
+            scene.cameras.main.setBounds(0, 0, map.widthInPixels, map.heightInPixels);
+            scene.physics.world.setBounds(0, 0, map.widthInPixels, map.heightInPixels);
 
             const music = scene.sound.add('bgm', { loop: true });
             music.play();
 
-            const mySquad = new Squad('mine');
+            const mySquad = new Squad(scene, 'mine');
             mySquad.add(
                 new Character(scene, 0, '0', { x: 300, y: 400 }),
                 new Character(scene, 1, '1', { x: 350, y: 400 }),
@@ -30,6 +32,21 @@ const Stage = () => {
                 new Character(scene, 4, '4', { x: 500, y: 400 }),
             );
             bindSquad(scene, mySquad);
+
+            const yourSquad = new Squad(scene, 'yours');
+            yourSquad.add(
+                new Character(scene, 56, '56', { x: 800, y: 800 }),
+                new Character(scene, 57, '57', { x: 850, y: 800 }),
+                new Character(scene, 58, '58', { x: 900, y: 800 }),
+                new Character(scene, 59, '59', { x: 950, y: 800 }),
+                new Character(scene, 60, '60', { x: 1000, y: 800 }),
+            );
+
+            scene.physics.add.collider(mySquad.group, yourSquad.group, (lhs, rhs) => {
+                console.log('hit');
+                scene.physics.world.separate(lhs.body as Phaser.Physics.Arcade.Body, rhs.body as Phaser.Physics.Arcade.Body);
+            });
+
         });
     }, []);
 
